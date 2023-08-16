dca7955c6a505c35d3723ed9fc554e5693a70a64
Logan <gelogan@uchicago.edu>
2023-08-16 16:23:06 -0400

    url request func

--- src/vectorstore/build_db.py
+++ src/vectorstore/build_db.py
@@ -3,6 +3,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from bs4 import BeautifulSoup
+import requests
 
 
 DATA_PATH = "vectorstore"
@@ -12,7 +13,8 @@ CHUNK_OVERLAP = 50
 
 def store_faiss_db(tab_id, content):
     # Store HTML content to FAISS vectorstore
-    soup = BeautifulSoup(content)
+    response = requests.get(content)
+    soup = BeautifulSoup(response, 'html.parser')
     text = soup.text
     
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE,
@@ -24,3 +26,7 @@ def store_faiss_db(tab_id, content):
 
     vectorstore = FAISS.from_documents(texts, embeddings)
     vectorstore.save_local(f'vectorstore/{tab_id}')
+    
+    
+if __name__ == "__main__":
+    
