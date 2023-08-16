1a5cead989a979a4ef3cd61c9ef3c812e1993b43
Hongyi Wu <hongyiwu@Hongyis-MacBook-Pro.local>
2023-08-16 14:44:29 -0400

    update api

--- src/send.py
+++ src/send.py
@@ -3,13 +3,23 @@ import requests
 send = Blueprint('send', __name__, url_prefix="/send")
 from datetime import datetime
 
-@send.post('/text')
-def send_text():
+@send.post('/chat')
+def send_chat():
     
 
             
     return "send"
 
+
+@send.post('/tab')
+def send_chat():
+    # Get the tab ID and content from the request parameters
+    tab_id = request.json['tab_id']
+    content = request.json['content']
+    
+
+    # Return a response
+    return ",".join([tab_id,content])
 
 
     
